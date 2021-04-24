02d2d9d5e515cfa7995c5575c4eb891393aecb20
Hellen <hellenfabiola2000@gmail.com>
2021-04-24 11:05:02 -0600

    Plantilla inicial

---
+++ WebsolsPrincipal/App_Start/BundleConfig.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Web.Optimization
+
+Public Module BundleConfig
+    ' Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+    Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+
+        bundles.Add(New ScriptBundle("~/bundles/jquery").Include(
+                    "~/Scripts/jquery-{version}.js"))
+
+        bundles.Add(New ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Scripts/jquery.validate*"))
+
+        ' Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+        ' para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                    "~/Scripts/modernizr-*"))
+
+        bundles.Add(New ScriptBundle("~/bundles/bootstrap").Include(
+                  "~/Content/assets/js/jquery.js",
+                  "~/Content/assets/js/bootstrap.min.js",
+                  "~/Content/assets/js/bootsnav.js",
+                  "~/Content/assets/js/owl.carousel.min.js",
+                  "~/Content/assets/js/custom.js"
+))
+
+        bundles.Add(New StyleBundle("~/Content/css").Include(
+                  "~/Content/assets/logo/favicon.png",
+                  "~/Content/assets/css/font-awesome.min.css",
+                  "~/Content/assets/css/linearicons.css",
+                  "~/Content/assets/css/animate.css",
+                  "~/Content/assets/css/owl.carousel.min.css",
+                  "~/Content/assets/css/owl.theme.default.min.css",
+                  "~/Content/assets/css/bootstrap.min.css",
+                  "~/Content/assets/css/bootsnav.css",
+                  "~/Content/assets/css/style.css",
+                  "~/Content/assets/css/responsive.css"
+))
+    End Sub
+End Module
+
---
+++ WebsolsPrincipal/Views/Shared/_Layout.vbhtml
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Websols</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+    @Styles.Render("~/Content/css")
+    @Scripts.Render("~/bundles/modernizr")
+</head>
+<body>
+    @Html.Partial("_Navbar")
+    @RenderBody()
+    @Html.Partial("_Footer")
+
+    @Scripts.Render("~/bundles/jquery")
+    @Scripts.Render("~/bundles/bootstrap")
+   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+</body>
+</html>
